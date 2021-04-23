fb7bad3563f4d75210ee0d2e4f8956d10234d832
Edwin Tumbaga <edwintumbaga@Edwins-MacBook-Pro.local>
2021-04-23 19:57:11 +0800

    Create constants file for API URL's. Add constants BREED_URL and CAT_IMAGES_URL

---
+++ src/apiConstants.js
@@ -0,0 +1,6 @@
+// API that gets all breeds
+export const BREED_URL='https://api.thecatapi.com/v1/breeds'; 
+
+// API that gets all images for specific cats 
+// Full syntax is https://api.thecatapi.com/v1/images/search?page=<page_number>&limit=<limit_number>&breed_id=<breed_id>
+export const CAT_IMAGES_URL='https://api.thecatapi.com/v1/images/search?'; 
