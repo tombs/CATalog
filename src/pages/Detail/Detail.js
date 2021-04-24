e3c61ce493d60bc03e17f7f991818c6738d5aae8
Edwin Tumbaga <edwintumbaga@Edwins-MacBook-Pro.local>
2021-04-24 20:53:54 +0800

    Update DetailsPage to reflect fetched cat details, including picture and description

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -7,33 +7,59 @@ import { getCatDetails } from 'scripts/dataFetch';
 import { CAT_DETAILS_URL } from 'apiConstants';
 
 // React Bootstrap Components
-import Container from 'react-bootstrap/Container'
+import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 class DetailsPage extends React.Component {
         constructor(props) {
             super(props);
             this.state = {
-                catDetails: ''
+                catDetailsResponse: '',
+                catDetails: '',
+                backUrl: '',
             };
         
           }
 
           componentDidMount() {
             const  { catId } = this.props.match.params;
-            console.log("search: "+catId);
+            console.log("search: "+catId);            
             
             getCatDetails(CAT_DETAILS_URL, catId).then(response => {                
                 this.setState({
-                    catDetails: response
+                    catDetailsResponse: response,
+                    catDetails: response.breeds[0]
                 })
               }
         );
           }
 
           render() {
+            const {
+              catDetailsResponse,
+              catDetails
+            } = this.state;
+
               return (
                 <Container>
-                    <h1> Welcome to the Details Page </h1>
+                  
+                  <Card>
+                    <Card.Header>
+                      <Button variant="primary" size="lg" href={'/?breed='+catDetails.id} active>
+                      Back
+                      </Button>
+                  </Card.Header>
+                   <Card.Img variant="top" src={catDetailsResponse.url} />
+                    <Card.Body>
+                      <Card.Title>{catDetails.name}</Card.Title>
+                      <Card.Text>{catDetails.origin}</Card.Text>
+                      <Card.Text>{catDetails.temperament}</Card.Text>
+                      <Card.Text>{catDetails.description}</Card.Text>
+                    </Card.Body>
+                  </Card>
+                          
               </Container>
               )                                
           }
