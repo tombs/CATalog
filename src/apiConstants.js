65c4d0c563068f7d600227f1f9f1fcf680834bec
Edwin Tumbaga <edwintumbaga@Edwins-MacBook-Pro.local>
2021-04-24 19:49:28 +0800

    Add new constant CAT_DETAILS_URL, a url for gettting cat details

--- src/apiConstants.js
+++ src/apiConstants.js
@@ -1,6 +1,10 @@
 // API that gets all breeds
-export const BREED_URL='https://api.thecatapi.com/v1/breeds'; 
+export const BREEDS_URL='https://api.thecatapi.com/v1/breeds'; 
 
-// API that gets all images for specific cats 
-// Full syntax is https://api.thecatapi.com/v1/images/search?page=<page_number>&limit=<limit_number>&breed_id=<breed_id>
-export const CAT_IMAGES_URL='https://api.thecatapi.com/v1/images/search?'; 
+// API that gets all images for specific cat breeds
+// Full syntax is https://api.thecatapi.com/v1/images/search?page=<pageNumber>&limit=<limit>&breed_id=<breedId>
+export const CAT_BREED_IMAGES_URL='https://api.thecatapi.com/v1/images/search?'; 
+
+// API that gets all details for a specific cat
+// Full syntax is https://api.thecatapi.com/v1/images/<catId>
+export const CAT_DETAILS_URL='https://api.thecatapi.com/v1/images/';
