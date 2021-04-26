559e5166ce88da85f07ae633e01009b415560fa0
Edwin Tumbaga <edwintumbaga@Edwins-MacBook-Pro.local>
2021-04-26 09:09:07 +0800

    Integrate ESLint fixes to apiConstants.js (no semicolon)

--- src/apiConstants.js
+++ src/apiConstants.js
@@ -1,10 +1,10 @@
 // API that gets all breeds
-export const BREEDS_URL='https://api.thecatapi.com/v1/breeds'; 
+export const BREEDS_URL = 'https://api.thecatapi.com/v1/breeds'
 
 // API that gets all images for specific cat breeds
 // Full syntax is https://api.thecatapi.com/v1/images/search?page=<pageNumber>&limit=<limit>&breed_id=<breedId>
-export const CAT_BREED_IMAGES_URL='https://api.thecatapi.com/v1/images/search?'; 
+export const CAT_BREED_IMAGES_URL = 'https://api.thecatapi.com/v1/images/search?'
 
 // API that gets all details for a specific cat
 // Full syntax is https://api.thecatapi.com/v1/images/<catId>
-export const CAT_DETAILS_URL='https://api.thecatapi.com/v1/images/';
+export const CAT_DETAILS_URL = 'https://api.thecatapi.com/v1/images/'
