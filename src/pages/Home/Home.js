f4504bfa8a0c44e29b59984661f4f4597404579a
Edwin Tumbaga <edwintumbaga@Edwins-MacBook-Pro.local>
2021-04-26 09:16:42 +0800

    Integrate ESLint fixes to Home.js (no semicolon, correct spacing, correct indentation, props validation)

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,207 +1,200 @@
-import React from 'react';
+import React from 'react'
+import PropTypes from 'prop-types'
 
 // Fetch data function (axios)
-import { getAllBreeds, getCatPicturesFromBreed } from 'scripts/dataFetch';
-import { BREEDS_URL, CAT_BREED_IMAGES_URL } from 'apiConstants';
+import { getAllBreeds, getCatPicturesFromBreed } from 'scripts/dataFetch'
+import { BREEDS_URL, CAT_BREED_IMAGES_URL } from 'apiConstants'
 
 // React Bootstrap Components
-import Form from 'react-bootstrap/Form';
-import CardDeck from 'react-bootstrap/CardDeck';
+import Form from 'react-bootstrap/Form'
+import CardDeck from 'react-bootstrap/CardDeck'
 import Container from 'react-bootstrap/Container'
 import Figure from 'react-bootstrap/Figure'
 
 // Custom Components
-import CatCard from 'components/Card';
-import LoadingButton from 'components/LoadButton';
+import CatCard from 'components/Card'
+import LoadingButton from 'components/LoadButton'
 
 const buttonStyle = {
-    width: "20%",
-};
+  width: '20%'
+}
 
 class HomePage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            catBreeds: [],
-            catBreedPics: [], // cat pic objects for display
-            fetchedPics:[], // latest fetched cat pic objects, which could include old cat pics
-            // newPics:[], // all pic objects from latest fetched pics that are not duplicates
-            currentPics:[], // list of id's of all currently displayed cat pics
-            currentBreed: null,
-            allCatsLoadad: false,
-            currentPage: 1,
-            totalCats: 0,
-            limitCount: 10,
-            isLoading: false,
-        };
-        this.getMoreCats = this.getMoreCats.bind(this);
-    
-      }
-
-    componentDidMount() {
-        const {
-            currentPage,
-            limitCount
-        } = this.state;
-        const queryString = this.props.location.search;
-        let searchParams = new URLSearchParams(queryString);
-        const breedId = searchParams.get('breed');
-        console.log("breedId: "+breedId);
-
-        getAllBreeds(BREEDS_URL).then(response => {                
-                this.setState({
-                    catBreeds: response
-                })
-              }
-        );
-
-        if (breedId) {
-            getCatPicturesFromBreed(CAT_BREED_IMAGES_URL,currentPage,limitCount,breedId)
-            .then(response => {
-                console.log("response: "+JSON.stringify(response));
-                this.setState(
-                        (state) => ({
-                            currentPage: state.currentPage + 1,
-                            fetchedPics: response,
-                            currentBreed: breedId
-                        }),
-                        () => {
-                            console.log("CALLBACK!!!");
-                            this.processCatPicID(response);
-                        }
-                        )
-            });    
-        }
-
+  constructor (props) {
+    super(props)
+    this.state = {
+      catBreeds: [],
+      catBreedPics: [], // cat pic objects for display
+      fetchedPics: [], // latest fetched cat pic objects, which could include old cat pics
+      // newPics:[], // all pic objects from latest fetched pics that are not duplicates
+      currentPics: [], // list of id's of all currently displayed cat pics
+      currentBreed: null,
+      allCatsLoadad: false,
+      currentPage: 1,
+      totalCats: 0,
+      limitCount: 10,
+      isLoading: false
     }
+    this.getMoreCats = this.getMoreCats.bind(this)
+  }
 
-    processCatPicID(catPicList) {
-        console.log("PROCESSING CATS!");
-        const {
-            currentPics
-        } = this.state;
+  componentDidMount () {
+    const {
+      currentPage,
+      limitCount
+    } = this.state
+    const queryString = this.props.location.search
+    const searchParams = new URLSearchParams(queryString)
+    const breedId = searchParams.get('breed')
+    console.log('breedId: ' + breedId)
 
-        let tempPicsList = [];
-        let tempNewPicsObjects = []
-        let newPicCounter = 0;
+    getAllBreeds(BREEDS_URL).then(response => {
+      this.setState({
+        catBreeds: response
+      })
+    }
+    )
 
-        catPicList.map(
-            catObj => {
-                if (currentPics.indexOf(catObj.id) === -1 ){
-                    tempPicsList.push(catObj.id);
-                    tempNewPicsObjects.push(catObj);
-                    newPicCounter+=1;
-                }
+    if (breedId) {
+      getCatPicturesFromBreed(CAT_BREED_IMAGES_URL, currentPage, limitCount, breedId)
+        .then(response => {
+          console.log('response: ' + JSON.stringify(response))
+          this.setState(
+            (state) => ({
+              currentPage: state.currentPage + 1,
+              fetchedPics: response,
+              currentBreed: breedId
+            }),
+            () => {
+              console.log('CALLBACK!!!')
+              this.processCatPicID(response)
             }
+          )
+        })
+    }
+  }
+
+  processCatPicID (catPicList) {
+    console.log('PROCESSING CATS!')
+    const {
+      currentPics
+    } = this.state
+
+    const tempPicsList = []
+    const tempNewPicsObjects = []
+    let newPicCounter = 0
+
+    catPicList.forEach(
+      catObj => {
+        if (currentPics.indexOf(catObj.id) === -1) {
+          tempPicsList.push(catObj.id)
+          tempNewPicsObjects.push(catObj)
+          newPicCounter += 1
+        }
+      }
+    )
+
+    if (newPicCounter > 0) {
+      this.setState(
+        (state) => ({
+          currentPics: state.currentPics.concat(tempPicsList),
+          catBreedPics: state.catBreedPics.concat(tempNewPicsObjects)
+          // newPics: state.newPics.concat(tempNewPicsObjects);
+        })
+      )
+    } else {
+      this.setState({
+        allCatsLoadad: true
+      })
+    }
+  }
+
+  getMoreCats () {
+    const {
+      currentPage,
+      limitCount,
+      currentBreed
+    } = this.state
+
+    this.setState({
+      isLoading: true
+    })
+
+    getCatPicturesFromBreed(CAT_BREED_IMAGES_URL, currentPage, limitCount, currentBreed)
+      .then(response => {
+        console.log('response: ' + JSON.stringify(response))
+        this.setState(
+          (state) => ({
+            currentPage: state.currentPage + 1,
+            fetchedPics: response,
+            isLoading: false
+          }),
+          () => {
+            console.log('CALLBACK!!! MORE CATS')
+            this.processCatPicID(response)
+          }
         )
+      })
+  }
 
+  handleChange (event, value) {
+    const {
+      currentPage,
+      limitCount
+    } = this.state
 
-        if (newPicCounter > 0) {
-            this.setState(
-                (state) => ({
-                    currentPics: state.currentPics.concat(tempPicsList),
-                    catBreedPics: state.catBreedPics.concat(tempNewPicsObjects),
-                    // newPics: state.newPics.concat(tempNewPicsObjects);
-                })
-                )
+    console.log('EVENT: ' + event)
+    console.log('VALUE: ' + event.target.value)
+    this.setState({
+      currentBreed: event.target.value
+    })
 
-        }
-        else {
-            this.setState({
-                allCatsLoadad: true
-            })
-        }
+    this.resetImages()
 
+    if (event.target.value === 'Select Breed') {
+      return
     }
 
-    getMoreCats() {
-        const {
-            currentPage,
-            limitCount, 
-            currentBreed
-        } = this.state;
+    getCatPicturesFromBreed(CAT_BREED_IMAGES_URL, currentPage, limitCount, event.target.value)
+      .then(response => {
+        console.log('CHANGE response: ' + JSON.stringify(response))
+        this.setState(
+          (state) => ({
+            currentPage: state.currentPage + 1,
+            fetchedPics: response
+          }),
+          () => {
+            console.log('CALLBACK!!!')
+            this.processCatPicID(response)
+          }
+        )
+      })
+  }
 
-        this.setState({
-            isLoading: true
-        });
+  // Remove images (reset state) when there is a change in breed selection
+  resetImages () {
+    this.setState({
+      catBreedPics: [],
+      totalCats: 0,
+      currentPage: 1,
+      currentPics: [],
+      allCatsLoadad: false,
+      fetchedPics: []
+    })
+  }
 
-        getCatPicturesFromBreed(CAT_BREED_IMAGES_URL,currentPage,limitCount,currentBreed)
-        .then(response => {
-            console.log("response: "+JSON.stringify(response));
-            this.setState(
-                (state) => ({
-                    currentPage: state.currentPage + 1,
-                    fetchedPics: response,
-                    isLoading: false,
-                }),
-                () => {
-                    console.log("CALLBACK!!! MORE CATS");
-                    this.processCatPicID(response);
-                }
-                )
-        });    
-    }
+  render () {
+    const {
+      catBreeds,
+      allCatsLoadad,
+      catBreedPics,
+      currentBreed,
+      isLoading
+    } = this.state
 
-    handleChange(event, value) {
-        const {
-            currentPage,
-            limitCount
-        } = this.state;
-
-        console.log("EVENT: "+event);
-        console.log("VALUE: "+event.target.value);
-        this.setState({
-            currentBreed: event.target.value
-        })
-        
-        this.resetImages()
-        
-        if (event.target.value === 'Select Breed') {
-            return;
-        }
-
-        getCatPicturesFromBreed(CAT_BREED_IMAGES_URL,currentPage,limitCount,event.target.value)
-        .then(response => {
-            console.log("CHANGE response: "+JSON.stringify(response));
-            this.setState(
-                (state) => ({
-                    currentPage: state.currentPage + 1,
-                    fetchedPics: response,
-                }),
-                () => {
-                    console.log("CALLBACK!!!");
-                    this.processCatPicID(response);
-                }
-                )
-                
-        });
-    }
-
-    // Remove images (reset state) when there is a change in breed selection
-    resetImages() {
-        this.setState({
-            catBreedPics : [],
-            totalCats: 0,
-            currentPage: 1,
-            currentPics: [],
-            allCatsLoadad: false,
-            fetchedPics: []
-        })
-    }
-
-      
-    render() {
-        const {
-            catBreeds,
-            allCatsLoadad,
-            catBreedPics,
-            currentBreed,
-            isLoading,
-        } = this.state;
-
-        console.log("catBreeds: "+JSON.stringify(catBreeds));
-        console.log("catBreedPics: "+JSON.stringify(catBreedPics));
-      return (
+    console.log('catBreeds: ' + JSON.stringify(catBreeds))
+    console.log('catBreedPics: ' + JSON.stringify(catBreedPics))
+    return (
           <Container>
             <h1>CAT-alogue</h1>
             <Figure>
@@ -215,8 +208,8 @@ class HomePage extends React.Component {
 
             <h4>Breed</h4>
             <Form>
-                <Form.Group controlId="exampleForm.SelectCustom">                    
-                    <Form.Control style={buttonStyle} as="select" value={currentBreed?currentBreed:''} custom onChange={(e, value)=>this.handleChange(e, value)}>
+                <Form.Group controlId="exampleForm.SelectCustom">
+                    <Form.Control style={buttonStyle} as="select" value={currentBreed || ''} custom onChange={(e, value) => this.handleChange(e, value)}>
                         <option key="breed" >Select Breed</option>
                         {catBreeds.map((breed) => (
                             <option key={breed.id} value={breed.id}>{breed.name}</option>
@@ -228,18 +221,25 @@ class HomePage extends React.Component {
       {catBreedPics.length > 0 && catBreedPics.map((cat) => {
         return (
             <CatCard picture={cat.url} catId={cat.id} key={cat.id} />
-        );
-      })}      
+        )
+      })}
     </CardDeck>
     { catBreedPics.length === 0 && (<h5>No Cats Available</h5>)}
 
-
-        { !allCatsLoadad &&  (<LoadingButton isCatLoading={isLoading    } noCats={catBreedPics.length===0?true:false} getCats={this.getMoreCats} />) }
-
+        { !allCatsLoadad && (<LoadingButton isCatLoading={isLoading } noCats={catBreedPics.length === 0} getCats={this.getMoreCats} />) }
 
         </Container>
-        );
-    }
+    )
   }
+}
 
-export default HomePage;
+// Props validation
+HomePage.propTypes = {
+
+  location: PropTypes.shape({
+    search: PropTypes.string
+  })
+
+}
+
+export default HomePage
