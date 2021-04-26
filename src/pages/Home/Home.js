08eb7bdeecd94e2851fc95bd0da178c6cf4ae63f
Edwin Tumbaga <edwintumbaga@Edwins-MacBook-Pro.local>
2021-04-26 11:35:22 +0800

    Add descriptive comments on functions and lengthy code parts,for clarity

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -15,6 +15,7 @@ import Figure from 'react-bootstrap/Figure'
 import CatCard from 'components/Card'
 import LoadingButton from 'components/LoadButton'
 
+// Custom style for Form Control
 const buttonStyle = {
   width: '20%'
 }
@@ -44,24 +45,28 @@ class HomePage extends React.Component {
       currentPage,
       limitCount
     } = this.state
+
+    // Get the breed ID from URL if coming from DetailPage
     const queryString = this.props.location.search
     const searchParams = new URLSearchParams(queryString)
     const breedId = searchParams.get('breed')
     console.log('breedId: ' + breedId)
 
-    getAllBreeds(BREEDS_URL).then(response => {
-      this.setState({
-        catBreeds: response
+    // Get all cat breeds using API
+    getAllBreeds(BREEDS_URL)
+      .then(response => {
+        this.setState({
+          catBreeds: response
+        })
       })
-    }
-    )
       .catch((error) => {
         console.log('ERROR: ' + JSON.stringify(error))
         this.setState({
           isApiError: true
         })
       })
-
+      
+    // If breed ID is present in query parameters, call cat images from breed
     if (breedId) {
       getCatPicturesFromBreed(CAT_BREED_IMAGES_URL, currentPage, limitCount, breedId)
         .then(response => {
@@ -87,6 +92,11 @@ class HomePage extends React.Component {
     }
   }
 
+  // Function to process incoming cat pictures from list <catPicList>.  
+  // functions include: 
+  //  - Old pictures are ignored (API returnes duplicate pictures)
+  //  - New pictures are added to the current list of pictures
+  //  - If no new pictures received (all are duplicates), remove "Load More" button (allCatsLoadad)
   processCatPicID (catPicList) {
     console.log('PROCESSING CATS!')
     const {
@@ -97,6 +107,8 @@ class HomePage extends React.Component {
     const tempNewPicsObjects = []
     let newPicCounter = 0
 
+    // Process each picture. New cats are added to tempNewPicObjects
+    // NewPicCounter is incremented for every new pic.
     catPicList.forEach(
       catObj => {
         if (currentPics.indexOf(catObj.id) === -1) {
@@ -107,6 +119,7 @@ class HomePage extends React.Component {
       }
     )
 
+    // If there are new cat pics, add them to the current list of cats (currentPics)
     if (newPicCounter > 0) {
       this.setState(
         (state) => ({
@@ -115,13 +128,15 @@ class HomePage extends React.Component {
           // newPics: state.newPics.concat(tempNewPicsObjects);
         })
       )
-    } else {
+    } else { // All cats have been loaded, remove "Load More" button
       this.setState({
         allCatsLoadad: true
       })
     }
   }
 
+  // This function is called by clicking the "Load More" button.
+  // Executes API to get more cats from a selected breed
   getMoreCats () {
     const {
       currentPage,
@@ -133,6 +148,7 @@ class HomePage extends React.Component {
       isLoading: true
     })
 
+    // Get more cats from the currently selected breed
     getCatPicturesFromBreed(CAT_BREED_IMAGES_URL, currentPage, limitCount, currentBreed)
       .then(response => {
         console.log('response: ' + JSON.stringify(response))
@@ -148,7 +164,7 @@ class HomePage extends React.Component {
           }
         )
       })
-      .catch((error) => {
+      .catch((error) => { // Handle API errors
         console.log('ERROR: ' + JSON.stringify(error))
         this.setState({
           isApiError: true
@@ -156,6 +172,8 @@ class HomePage extends React.Component {
       })
   }
 
+  // Function handles changes in the breed select button.
+  // Executes API to get all cat pictures from selected breed
   handleChange (event, value) {
     const {
       currentPage,
@@ -168,12 +186,14 @@ class HomePage extends React.Component {
       currentBreed: event.target.value
     })
 
+    // Reset images and other states whenever a new breed is selected.
     this.resetImages()
 
     if (event.target.value === 'Select Breed') {
       return
     }
 
+    // Execute API to get all pictures from the selected breed
     getCatPicturesFromBreed(CAT_BREED_IMAGES_URL, currentPage, limitCount, event.target.value)
       .then(response => {
         console.log('CHANGE response: ' + JSON.stringify(response))
@@ -196,7 +216,8 @@ class HomePage extends React.Component {
       })
   }
 
-  // Remove images (reset state) when there is a change in breed selection
+  // Remove images (reset states) when there is a change in breed selection.
+  // Called everytime there is a change in breed selection.
   resetImages () {
     this.setState({
       catBreedPics: [],
